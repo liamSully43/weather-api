7b8551bce9f62b23d10f1ea63f152bb97a02d500
liamSully43 <60324016+liamSully43@users.noreply.github.com>
2020-05-10 20:29:12 +0100

    V1.1

---
+++ precache-manifest.1a8000d65a25cc571a7f67f66f201980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b0c238724c2657c6caf0b45983451e",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "d9ad8fc8702d73f7bfd8",
+    "url": "/weather-app/static/css/main.0f3230b5.chunk.css"
+  },
+  {
+    "revision": "b78366e5c4a699672e25",
+    "url": "/weather-app/static/js/2.42b4fe0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.42b4fe0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9ad8fc8702d73f7bfd8",
+    "url": "/weather-app/static/js/main.89bd30ec.chunk.js"
+  },
+  {
+    "revision": "012abc6174db71a893dc",
+    "url": "/weather-app/static/js/runtime-main.938664af.js"
+  }
+]);
